01e103b4f3999582f07b76a5f44cf96bb2899f62
Rifat Hossain <rifat.hossain.it@gmail.com>
2024-08-19 01:29:00 -0400

    Version 1 restored

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
 console.log('config.js');
 console.log ('change 2')
-console.log('Version 2');
-console.log('Version 3');
--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 console.log('index.js');
 console.log ('change 2')
-console.log('Version 2');
-console.log('Version 3');
